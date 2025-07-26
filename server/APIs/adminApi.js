182dc13c25542d34d8596885c9b85e47f31faf58
jagan rao <23071a6927@vnrvjiet.in>
2025-07-26 10:35:17 +0530

    updated adminapi to get data from the google

--- server/APIs/adminApi.js
+++ server/APIs/adminApi.js
@@ -7,11 +7,28 @@ const admin =require( '../models/adminModel');
 
 // In adminApi.js
 adminApp.post('/login', expressAsyncHandler(async (req, res) => {
-  const { email } = req.body;
+  const { email, profileImageUrl, firstName, lastName, role  } = req.body;
   const adminUser = await admin.findOne({ email });
   if (!adminUser) {
     return res.status(401).send({ message: 'Invalid admin credentials' });
   }
+
+  // Update the admin document in the database with the latest info from Clerk/frontend
+  // This ensures profileImageUrl (and other fields) are always synced
+  const updateFields = {};
+  if (profileImageUrl) updateFields.profileImageUrl = profileImageUrl;
+  if (firstName) updateFields.firstName = firstName;
+  if (lastName) updateFields.lastName = lastName;
+  if (role) updateFields.role = role; // Ensure role is 'admin'
+
+  if (Object.keys(updateFields).length > 0) {
+    adminUser = await admin.findOneAndUpdate(
+      { email },
+      { $set: updateFields }, // Use $set to update specific fields
+      { new: true, runValidators: true } // Return the updated document and run schema validators
+    );
+  }
+  
   res.send({ message: 'admin', payload: adminUser });
 }));
 
